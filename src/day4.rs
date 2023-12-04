c5d602891a1f5bff11dbfbc8d3e51351991714d2
Baptiste Manson <baptiste.manson@gmail.com>
2023-12-04 13:35:27 +0100

    day4

---
+++ src/day4.rs
@@ -0,0 +1,46 @@
+use std::{collections::HashSet, fs::read_to_string};
+
+// 39296 too high
+// 24542
+pub fn pb1() {
+    let mut sum = 0;
+    for l in read_to_string("./src/day4.input.txt").unwrap().lines() {
+        let numbers = l.split_once(":").unwrap().1;
+        let score = nb_win(numbers);
+        if score > 0 {
+            sum += 1 << score - 1;
+        }
+    }
+    dbg!(sum);
+}
+
+fn nb_win(numbers: &str) -> usize {
+    let (winning_nb, played_nb) = numbers.split_once("|").unwrap();
+    let winning_nb: HashSet<&str> = parse(winning_nb);
+    let played_nb: HashSet<&str> = parse(played_nb);
+    played_nb.intersection(&winning_nb).count()
+}
+
+fn parse(winning_nb: &str) -> HashSet<&str> {
+    winning_nb
+        .trim()
+        .split(" ")
+        .filter(|a| a.len() > 0)
+        .collect()
+}
+// 229 too low
+// 8736438
+pub fn pb2() {
+    let mut stock = vec![1; 205];
+    for (i, l) in read_to_string("./src/day4.input.txt")
+        .unwrap()
+        .lines()
+        .enumerate()
+    {
+        let numbers = l.split_once(":").unwrap().1;
+        for x in 1..=nb_win(numbers) {
+            stock[i + x] += stock[i];
+        }
+    }
+    dbg!(stock.iter().sum::<i32>());
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod data3;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod debug;
 mod playground;
 use thousands::Separable;
@@ -24,6 +25,8 @@ fn execute(pb: String) {
         "22" => day2::pb2(),
         "31" => day3::pb1(),
         "32" => day3::pb2(),
+        "41" => day4::pb1(),
+        "42" => day4::pb2(),
         "playground" => playground::main(),
         _ => panic!("unknown problem"),
     };
